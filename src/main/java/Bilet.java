1c4eee18fbd808633c305d25d07d8cb5833c9014
ekimaycicek <ekimaycicek@icloud.com>
2023-09-17 20:31:51 +0300

    new

--- src/main/java/Bilet.java
+++ src/main/java/Bilet.java
@@ -103,6 +103,7 @@ public class Bilet {
 
                     } else System.out.println("please indicate your direction");
 
+
                 } else if (route.equals("C")) {
                     if (flightDirection == 1) {
                         System.out.println(" *** ");
